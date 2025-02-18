45b8183bcd3b4a8cf80455a986c9d6188ef0fb1e
idanharari11 <idanharari112@gmail.com>
2025-02-18 16:55:41 +0200

    Optimize WhatsApp icon with Next.js Image component

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import { HeroSection } from "@/components/sections/hero-section"
 import { CryptoCharts } from '@/components/sections/crypto-charts'
 import { ChecklistFeatures } from '@/components/sections/checklist-features'
 import ContactForm from '@/components/sections/contact-form'
+import Image from "next/image"
 
 export default function Page() {
   const navItems = [
@@ -19,7 +20,7 @@ export default function Page() {
     <main id="home" className="min-h-screen">
       <div className="whatsapp-icon-container fixed top-0 left-0 m-2 text-center cursor-pointer">
         <span className="block text-black pl-2 font-bold">דברו איתנו כאן!</span>
-        <img src="/whatsapp-image.png" alt="WhatsApp" className="whatsapp-icon" />
+        <Image width={90} height={70} src="/whatsapp-image.png" alt="WhatsApp" className="whatsapp-icon" />
       </div>
       <div className="fixed top-0 right-0 m-2 text-black font-bold company-number">
         <span className="underline">מספר חברה:</span>
